62bb312e87bbe2897907ac5d277d2c1364cf5426
“MemoOrtizgit config --global user.email “memodor2001@gmail.com <memodor2001@gmail.com>
2024-08-09 11:17:26 -0600

    Add pandasLearning.py with initial Pandas learning progress
    Added pandasLearning.py to document initial exploration and practice
    with Pandas functionalities. This script demonstrates basic operations
    for creating and inspecting DataFrames.
    
    - Imported Pandas and displayed version
    - Created a DataFrame with specified columns and indices
    - Utilized functions to explore DataFrame properties such as head, tail,
      columns, index, values, shape, and info
    
    This addition provides foundational examples for working with data
    using Pandas, supporting further learning and experimentation.

--- pandasLearning.py
+++ pandasLearning.py
@@ -1,3 +1,19 @@
 import pandas as pd
 
+
 print(pd.__version__)
+
+#Crear un dataframe
+#Puedes agregar parametros para los datos, columnas e indices
+df = pd.DataFrame([[1,2,3],[4,5,6],[7,8,9]], columns= ['A','B','C'], index=['x','y','z'])
+
+print(df)
+
+df.head()
+df.tail()
+df.columns
+df.index
+df.values
+df.shape
+
+df.info()
