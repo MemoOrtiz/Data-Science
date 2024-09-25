8d0c6e4e747161d2bea64f0a8ad31f2f0d145189
“MemoOrtizgit config --global user.email “memodor2001@gmail.com <memodor2001@gmail.com>
2024-09-25 17:42:44 -0600

    Se añadio el archivo DataScienceCourse.py para el curso de Ciencia de Datos en el ITS

---
+++ DataScienceCourse.py
@@ -0,0 +1,33 @@
+import math as m
+
+#Math expressions: Familiar operators
+a = 2+2
+print(a)
+
+d = 4 //3
+print(d)
+
+e = 4/3
+print(e)
+
+f = 4*3
+print(f)
+
+g = 4-3
+print(g)
+
+h = 4**3
+print(h)
+
+i = m.sqrt(16)
+print(i)
+
+j = round(3.14159, 2)
+print(j)
+
+k = m.factorial(5)
+print(k)
+
+l = abs(-5)
+print(l)
+
